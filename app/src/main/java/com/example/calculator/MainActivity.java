5a0491cfd491cada03426f02206603f5a83173f5
Nahid2194 <nahidjc80@gmail.com>
2021-02-02 20:46:40 +0600

    onClick Function add

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void divFunction(View view) {
+    }
+
+    public void plusFunction(View view) {
+    }
+
+    public void subFunction(View view) {
+    }
+
+    public void mulFunction(View view) {
     }
 }
